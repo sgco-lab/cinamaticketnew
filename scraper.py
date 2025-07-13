6df6cd3f8252f5a274a3048acc2205835470fce5
sgco-lab <sepahangostarco@gmail.com>
2025-07-13 17:40:24 +0330

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 import os
@@ -10,9 +11,8 @@ def scrape_samfaa():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.binary_location = "/usr/bin/chromium-browser"  # اضافه کردن مسیر باینری مرورگر
 
-    driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
         driver.get("https://www.samfaa.ir/")
